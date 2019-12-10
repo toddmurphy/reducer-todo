9725b56b37f461f60299a072dd6089d2862f7026
toddmurphy <toddmurphy93@gmail.com>
2019-12-10 19:13:11 -0400

    trying to get basic reducer structure setup and component structure setup

--- reducer-todo-app/src/App.js
+++ reducer-todo-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
       <div>
         <h1>Reducer todo project</h1>
+        <Todos />
       </div>
     </div>
   );
---
+++ reducer-todo-app/src/components/Todos.js
@@ -0,0 +1,32 @@
+import React, { useState, useReducer } from 'react';
+
+import { reducer, initialState } from '../reducers/todoReducer';
+
+const Todos = () => {
+
+    //reducer hook --> need to import reducer and initialState
+    const [todoState, dispatch] = useReducer(reducer, initialState)
+
+    const [newTodo, setNewTodo] = useState('');
+
+    //setup 'handleInputChanges' to watch for changes 'newTodo'
+    const handleInputChanges = (event) => {
+        setNewTodo(event.target.value);
+    }
+
+    return (
+        <div>
+            <h3>Todo component --> add single todo here</h3>
+            <form>
+                <input
+                    type='text'
+                    placeholder='Add todo'
+                    onChange={handleInputChanges}
+                />
+                <button>Add todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default Todos;
